34c33684093750e82039666a4e190addcf1b1075
GongTommy <04031991ttkl>
2017-10-22 18:46:39 +0800

    更新目录

---
+++ app/src/main/java/com/example/administrator/testas2_3/TestEntity.java
@@ -0,0 +1,26 @@
+package com.example.administrator.testas2_3;
+
+/**
+ * Created by Administrator on 2017/10/22.
+ */
+
+public class TestEntity {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
