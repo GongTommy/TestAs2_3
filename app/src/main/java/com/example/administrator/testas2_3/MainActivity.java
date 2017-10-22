f1c97805e9408d1fc040d7a4001e276839d330a4
GongTommy <04031991ttkl>
2017-10-22 17:47:57 +0800

    提交测试

---
+++ app/src/main/java/com/example/administrator/testas2_3/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.administrator.testas2_3;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+public class MainActivity extends Activity {
+    private ListView lv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/administrator/testas2_3/TestApplication.java
@@ -0,0 +1,10 @@
+package com.example.administrator.testas2_3;
+
+import android.app.Application;
+
+/**
+ * Created by Administrator on 2017/10/1.
+ */
+
+public class TestApplication extends Application {
+}
